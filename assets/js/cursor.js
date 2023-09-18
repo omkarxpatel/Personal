0df5065c7faf186452dba75231cc652769fcb908
omkarxpatel <bendonveryt@gmail.com>
2023-09-17 20:12:58 -0700

    test

--- assets/js/cursor.js
+++ assets/js/cursor.js
@@ -2,7 +2,7 @@
 const circle = document.getElementById('follower');
 const delay = 100; // Adjust this value for the delay (in milliseconds)
 
-document.addEventListener('mousemove', (e) => {
+function updateCursorStyle(e) {
     // Calculate the new position with a delay
     setTimeout(() => {
         const x = e.clientX;
@@ -10,4 +10,13 @@ document.addEventListener('mousemove', (e) => {
         circle.style.left = x + 'px';
         circle.style.top = y + 'px';
     }, delay);
-});
+
+    // Check if the device has a pointer (e.g., a mouse)
+    if (window.matchMedia('(pointer: fine)').matches) {
+        circle.classList.add('outline-cursor');
+    } else {
+        circle.classList.remove('outline-cursor');
+    }
+}
+
+document.addEventListener('mousemove', updateCursorStyle);
